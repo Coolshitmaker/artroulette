80ba9d9cba995b4936557f3cce37e3184a4b1896
Sergej Subotic <sergejsubotic16@gmail.com>
2025-05-15 20:19:50 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css' 
+import '../styles/welcome.css';
+import '../styles/works.css'
+import '../styles/cards.css'
+import '../styles/commissions.css'
+import '../styles/tos.css'
+import '../styles/mobilenav.css'
+import '../styles/utility.css'
+
+import mobileNav from './utils/mobile-nav';
+
+mobileNav();
